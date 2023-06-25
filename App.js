c6b80aa0fc27839a282fed61fdd8ab5395bf531b
Jaycee <jayceefelix26@gmail.com>
2023-06-25 01:09:19 -0300

    Stack navigation installations and imports

--- App.js
+++ App.js
@@ -1,5 +1,11 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
+
 
 export default function App() {
   return (
